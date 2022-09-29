23529e7ab133f62e09fca4a29bec9d8502ea1386
IsuruGithu <lakmalai72@gmail.com>
2022-09-29 13:46:40 +0530

    2nd upload

--- main.cpp
+++ main.cpp
@@ -24,4 +24,80 @@ char car[4][4] = { ' ','+','+',' ',
 int carPos = WIN_WIDTH/2;
 int score = 0;
 
-void gotoxy
+void gotoxy(int x, int y){
+    CursorPosition.X = x;
+    CursorPosition.Y = y;
+    SetConsoleCursorPosition(console, CursorPosition);
+}
+
+void setcursor (bool visible, DWORD size){
+    if (size == 0)
+        size = 20;
+
+    CONSOLE_CURSOR_INFO lpcursor;
+    lpcursor.bVisible = visible;
+    lpcursor.dwSize = size;
+    setConsoleCursorInfo(console,&lpcursor);
+
+}
+
+void drawBorder(){
+    for (int i=0, i <SCREEN_HEIGHT; i++){
+        for (int j = 0; j < 17; j++) {
+            gotoxy(0+j,i); cout<<"+";
+            gotoxy(WIN_WIDTH-j,i); cout<<"+";
+        }
+    }
+    for (int i = 0; i < SCREEN_HEIGHT; i++) {
+        gotoxy(SCREEN_WIDTH,i); cout<< "+";
+    }
+}
+
+/*enemy--------------------------------------------------------*/
+
+void genEnemy(int ind){
+    enemyX[ind] = 17 + rand()%(33);
+}
+
+void drawEnemy(int ind){
+    if (enemyFlag[ind] == true ){
+        gotoxy(enemyX[ind], enemyY[ind]);          cout<<"****";
+        gotoxy(enemyX[ind], enemyY[ind]+1);     cout<<" ** ";
+        gotoxy(enemyX[ind], enemyY[ind]+2);     cout<<"****";
+        gotoxy(enemyX[ind], enemyY[ind]+3);     cout<<" ** ";
+    }
+}
+
+void eraseEnemy(int ind){
+    if (enemyFlag[ind] == true){
+        gotoxy(enemyX[ind], enemyY[ind]);       cout<<"    ";
+        gotoxy(enemyX[ind], enemyY[ind]+1);       cout<<"    ";
+        gotoxy(enemyX[ind], enemyY[ind]+2);       cout<<"    ";
+        gotoxy(enemyX[ind], enemyY[ind]+3);       cout<<"    ";
+    }
+}
+
+void resetEnemy(int ind){
+    eraseEnemy(ind);
+    enemyY[ind] = 1;
+    genEnemy(ind);
+}
+
+void drawCar(){
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            gotoxy(j+carPos, i+22); cout<<car[i][j];
+
+        }
+    }
+}
+
+void eraseCar(){
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            gotoxy(j+carPos, i+22); cout<<"  ";
+
+        }
+
+    }
+}
