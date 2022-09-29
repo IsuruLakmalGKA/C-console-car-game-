478f90b25095444747c8e386703c62102ba72a5d
IsuruGithu <lakmalai72@gmail.com>
2022-09-29 13:19:49 +0530

    first upload

---
+++ cmake-build-debug/CMakeFiles/Car_race_console_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Car_race_console_.dir/main.cpp.o"
+  "Car_race_console_"
+  "Car_race_console_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Car_race_console_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
